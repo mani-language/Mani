6029382414adad77b593116cc0c0f0f9d28fd960
crazywolf <crazywolf132@gmail.com>
2018-12-11 23:42:08 +0700

    Started adding the Atomic Ints

--- src/com/mani/lang/Modules/atomic/atomic.java
+++ src/com/mani/lang/Modules/atomic/atomic.java
@@ -10,11 +10,19 @@ public final class atomic implements Module {
 
     @Override
     public void init(Interpreter interpreter) {
+        // Everything for the atomic booleans
         interpreter.addSTD("AtomicBool", new atomic_boolean());
         interpreter.addSTD("ABSet", new atomic_boolean_set());
         interpreter.addSTD("ABCompareSet", new atomic_boolean_compareSet());
         interpreter.addSTD("ABGetSet", new atomic_boolean_getSet());
         interpreter.addSTD("ABGet", new atomic_boolean_get());
+
+        // Everything for the atomic integers
+        interpreter.addSTD("AtomicInt", new atomic_int());
+        //interpreter.addSTD("AISet", new atomic_int_set());
+        //interpreter.addSTD("AICompareSet", new atomic_int_compareSet());
+        //interpreter.addSTD("AIGet", new atomic_int_get());
+        interpreter.addSTD("AIGetAdd", new atomic_int_getAdd());
     }
 
 }
---
+++ src/com/mani/lang/Modules/atomic/atomic_int_getAdd.java
@@ -0,0 +1,40 @@
+package com.mani.lang.Modules.atomic;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.mani.lang.Interpreter;
+import com.mani.lang.ManiCallable;
+
+public final class atomic_int_getAdd implements ManiCallable {
+
+    @Override
+    public int arity() {
+        return 3;
+    }
+
+    @Override
+	public Object call(Interpreter interpreter, List<Object> arguments) {
+        /**
+         * Arguments:
+         * 1 -  The atomic int
+         * 2 -  Amount to add
+         * 3 -  Return prev
+         */
+
+        AtomicInteger val = (AtomicInteger) arguments.get(0);
+        Integer toAdd = (Integer) arguments.get(1);
+        boolean returnPrev = (boolean) arguments.get(2);
+
+        val.addAndGet(toAdd);
+
+        // A cheat way we can do to return the previous...
+        // is to minus the added from the current.
+        if (returnPrev) {
+            return val.get() - toAdd;
+        }
+
+		return val.get();
+	}
+
+}
