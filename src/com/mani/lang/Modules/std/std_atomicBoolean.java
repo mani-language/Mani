6a3dfc5157a036ad730caee8e24f4956e7f5261b
crazywolf <crazywolf132@gmail.com>
2018-12-11 18:19:06 +0700

    Starting work on adding atomic operators

---
+++ src/com/mani/lang/Modules/std/std_atomicBoolean.java
@@ -0,0 +1,16 @@
+package com.mani.lang.Modules.std;
+
+import java.util.List;
+
+import com.mani.lang.Interpreter;
+import com.mani.lang.ManiCallable;
+import com.mani.lang.Modules.Module;
+
+public final class std_atomicBoolean implements Module {
+
+    @Override
+    public void init(Interpreter interpreter) {
+
+    }
+
+}
