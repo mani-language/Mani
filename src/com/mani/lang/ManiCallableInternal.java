93337219020b1682dc9f4be555cb0b9b8f0fe818
crazywolf <crazywolf132@gmail.com>
2019-03-14 19:00:26 +1100

    Added extension type of Internal.
    These are only used for things such as `.at()` on list items.
    
    They will not be used like regular functions.

---
+++ src/com/mani/lang/ManiCallableInternal.java
@@ -0,0 +1,30 @@
+package com.mani.lang;
+
+import java.util.List;
+
+/*public interface ManiCallableInternal {
+    // arity is the amount of arguments the command needs.
+    Object workWith = null;
+    void setItem(Object item);
+    int arity();
+    Object call(Interpreter interpreter, List<Object> arguments);
+}*/
+
+public class ManiCallableInternal implements ManiCallable {
+
+    public Object workWith = null;
+
+    public void setItem(Object item) {
+        this.workWith = item;
+    }
+
+    @Override
+    public int arity() {
+        return 0;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        return null;
+    }
+}
