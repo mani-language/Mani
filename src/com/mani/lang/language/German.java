51a1ee9cbcb6e88d42313ee7f3d0a0e5cf859e69
crazywolf <crazywolf132@gmail.com>
2019-03-25 16:57:49 +1100

    Working on german translation:

--- src/com/mani/lang/language/German.java
+++ src/com/mani/lang/language/German.java
@@ -9,6 +9,8 @@ public class German implements Lang {
     @Override
     public Object init(Map<String, String> keywords) {
         keywords.put("say", "sagen");
+        keywords.put("break", "brechen");
+        keywords.put("return", "rückkehr");
         return keywords;
     }
 }
