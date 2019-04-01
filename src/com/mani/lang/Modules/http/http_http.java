d5de5dea3573c6b2b4de74cbf9a20a5e45b39049
crazywolf <crazywolf132@gmail.com>
2019-04-01 22:12:11 +1100

    http module basics

---
+++ src/com/mani/lang/Modules/http/http_http.java
@@ -0,0 +1,37 @@
+package com.mani.lang.modules.http;
+
+import com.mani.lang.Interpreter;
+import com.mani.lang.ManiCallable;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+/*import java.util.Map;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.internal.http.HttpMethod;*/
+
+public final class http_http implements ManiCallable {
+
+    @Override
+    public int arity() {
+        return 1;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        /*try {
+            final Response response = client.newCall(
+                    new Request.Builder().url(arguments.get(0).toString()).build())
+                    .execute()
+            return response.body().bytes();
+        } catch (IOException ex) {
+            return ex;
+        }*/
+        return null;
+    }
+}
