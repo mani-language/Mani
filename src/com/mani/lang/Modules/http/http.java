f485821c750fbefcead913605c8d85a9b9dbbe9d
crazywolf <crazywolf132@gmail.com>
2019-04-01 22:12:50 +1100

    http core module file.

---
+++ src/com/mani/lang/Modules/http/http.java
@@ -0,0 +1,19 @@
+package com.mani.lang.modules.http;
+
+import com.mani.lang.Interpreter;
+import com.mani.lang.ManiCallable;
+
+import java.util.List;
+
+public final class http implements ManiCallable {
+
+    @Override
+    public int arity() {
+        return 0;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        return null;
+    }
+}
