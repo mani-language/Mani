2567f7279aae494b2db01113fd8c55033a8b4ef1
crazywolf <crazywolf132@gmail.com>
2019-04-04 14:49:04 +1100

    Fixed internal error with index.

--- src/com/mani/lang/Modules/std/std_trim.java
+++ src/com/mani/lang/Modules/std/std_trim.java
@@ -14,7 +14,7 @@ public final class std_trim implements ManiCallable {
 
     @Override
 	public Object call(Interpreter interpreter, List<Object> arguments) {
-		if (arguments.size() != 1 || ( arguments.size() == 1 && !(arguments.get(1) instanceof String))) {
+		if (arguments.size() != 1 || ( arguments.size() == 1 && !(arguments.get(0) instanceof String))) {
             return "Please provide 1 arguement, a string";
         }
         return arguments.get(0).toString().trim();
