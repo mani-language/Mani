7e05e8327f3fea1fe3f73ce260efcf01afbff520
crazywolf <crazywolf132@gmail.com>
2019-04-18 08:31:52 +1000

    Allow file_read to take both String and file

--- src/com/mani/lang/Modules/files/files_read.java
+++ src/com/mani/lang/Modules/files/files_read.java
@@ -20,7 +20,12 @@ public class files_read implements ManiCallable {
     public Object call(Interpreter interpreter, List<Object> arguments) {
         try {
             String data = "";
-            File f = new File((String)arguments.get(0));
+            File f = null;
+            if (arguments.get(0) instanceof String) {
+                f = new File((String)arguments.get(0));
+            } else if (arguments.get(0) instanceof File) {
+                f = (File) arguments.get(0);
+            }
 
             Scanner fscan = new Scanner(f);
 
