aa1cfd43f95ef8947e85f381de37e75fe3e13201
crazywolf132 <crazywolf132@gmail.com>
2019-04-21 16:09:56 +1000

    Removed un-used imports from Mani

--- src/com/mani/lang/Mani.java
+++ src/com/mani/lang/Mani.java
@@ -1,6 +1,9 @@
 package com.mani.lang;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
