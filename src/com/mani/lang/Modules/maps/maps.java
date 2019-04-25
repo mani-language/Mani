8514692beb83662aa19ed60781dfdbe4d07eed77
crazywolf <crazywolf132@gmail.com>
2019-04-26 08:28:53 +1000

    Added base functions to standard maps
    As described in the docs, the maps class has the base functions. These were never actually in the language, but now they are.

--- src/com/mani/lang/Modules/maps/maps.java
+++ src/com/mani/lang/Modules/maps/maps.java
@@ -1,7 +1,12 @@
 package com.mani.lang.Modules.maps;
 
 import com.mani.lang.Interpreter;
+import com.mani.lang.ManiCallable;
+import com.mani.lang.ManiCallableInternal;
 import com.mani.lang.Modules.Module;
+
+import java.util.HashMap;
+import java.util.List;
 
 public final class maps implements Module {
 
@@ -19,12 +24,56 @@ public final class maps implements Module {
 
     @Override
     public boolean hasExtensions() {
-        return false;
+        return true;
     }
 
     @Override
     public Object extensions() {
-        return null;
+        HashMap<String, HashMap<String, ManiCallableInternal>> db = new HashMap<>();
+        HashMap<String, ManiCallableInternal> locals = new HashMap<>();
+
+        locals.put("count", new ManiCallableInternal() {
+            @Override
+            public int arity() { return 1; }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double) ((HashMap<Object, Object>) this.workWith).size();
+            }
+        });
+
+        locals.put("add", new ManiCallableInternal() {
+            @Override
+            public int arity() { return 2; }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return ((HashMap<Object, Object>) this.workWith).put(arguments.get(0), arguments.get(1));
+            }
+        });
+
+        locals.put("del", new ManiCallableInternal() {
+            @Override
+            public int arity() { return 1; }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return ((HashMap<Object, Object>) this.workWith).remove(arguments.get(0));
+            }
+        });
+
+        locals.put("at", new ManiCallableInternal() {
+           @Override
+           public int arity() { return 1; }
+
+           @Override
+           public Object call(Interpreter interpreter, List<Object> arguments) {
+               return ((HashMap<Object, Object>) this.workWith).get(arguments.get(0));
+           }
+        });
+
+        db.put("map", locals);
+        return db;
     }
 
 }
