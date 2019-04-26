5405e84f6466816fc779e78c03887e603d6a18a4
callcraft456@gmail.com <callcraft456@gmail.com>
2019-04-26 10:31:53 +0300

    Delete arg size checking

--- src/com/mani/lang/Modules/arrays/arrays_reverseArray.java
+++ src/com/mani/lang/Modules/arrays/arrays_reverseArray.java
@@ -15,13 +15,10 @@ public final class arrays_reverseArray implements ManiCallable {
 
     @Override
 	public Object call(Interpreter interpreter, List<Object> arguments) {
-		if (arguments.size() != 1) {
-            return "Please provide an array to reverse.";
-        }
         ArrayList<Object> arr = (ArrayList<Object>) arguments.get(0);
         ArrayList reversed = new ArrayList<>();
         int arrSize = arr.size();
-        
+
         for (int i = 0; i < arrSize; i++) {
           reversed.add(arr.get(arrSize - i - 1));
         }
