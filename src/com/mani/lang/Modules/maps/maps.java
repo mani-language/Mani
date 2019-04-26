8553f04b11c46ad9d9bed83084d4cbb071f5bbb6
callcraft456@gmail.com <callcraft456@gmail.com>
2019-04-26 09:26:00 +0300

    Added methods to core and stdlib

--- src/com/mani/lang/Modules/maps/maps.java
+++ src/com/mani/lang/Modules/maps/maps.java
@@ -13,13 +13,15 @@ public final class maps implements Module {
     @Override
     public void init(Interpreter interpreter) {
         interpreter.addSTD("newMap", new maps_newMap());
-        interpreter.addSTD("mapGetValue", new maps_mapGetValue()); 
+        interpreter.addSTD("mapGetValue", new maps_mapGetValue());
         interpreter.addSTD("mapAddItem", new maps_mapAddItem());
         interpreter.addSTD("mapRemoveItem", new maps_mapRemoveItem());
         interpreter.addSTD("mapUpdateItem", new maps_mapUpdateItem());
         interpreter.addSTD("mapGetKeys", new maps_mapGetKeys());
         interpreter.addSTD("arraysToMap", new maps_arraysToMap());
         interpreter.addSTD("mapKeyExists", new maps_mapKeyExists());
+        interpreter.addSTD("mapGetValues", new maps_mapGetValues());
+        interpreter.addSTD("mapFind", new maps_mapFind());
 	}
 
     @Override
