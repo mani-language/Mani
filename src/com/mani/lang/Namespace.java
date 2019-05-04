1b00b69ab56a08fa73c4adde8b942f4d6b7d56c0
crazywolf132 <crazywolf132@gmail.com>
2019-05-04 13:01:24 +1000

    Adding Namespace class.
    Was originally created by Kalekdan

---
+++ src/com/mani/lang/Namespace.java
@@ -0,0 +1,45 @@
+package com.mani.lang;
+
+public class Namespace {
+
+    private String space;
+    private String name;
+
+    Namespace(String space, String name) {
+        this.space = space;
+        this.name = name;
+    }
+
+    // These must be overriden to make sure the .equals and .containsKey functions work as expected
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((space == null) ? 0 : space.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return  result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Namespace other = (Namespace) obj;
+        if (space == null) {
+            if (other.space != null)
+                return false;
+        } else if (!space.equals(other.space))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+}
