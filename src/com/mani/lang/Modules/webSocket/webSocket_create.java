9a34f42b1db712e24badec6c6395a42702d51ccb
crazywolf132 <crazywolf132@gmail.com>
2019-05-09 13:31:34 +1000

    Creating a new websocket (Module)

---
+++ src/com/mani/lang/Modules/webSocket/webSocket_create.java
@@ -0,0 +1,26 @@
+package com.mani.lang.Modules.webSocket;
+
+import com.mani.lang.Interpreter;
+import com.mani.lang.ManiCallable;
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class webSocket_create implements ManiCallable {
+    @Override
+    public int arity() {
+        return 1;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        WebSocketFactory factory = new WebSocketFactory().setConnectionTimeout(5000);
+        try {
+            return factory.createSocket((String) arguments.get(0));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
