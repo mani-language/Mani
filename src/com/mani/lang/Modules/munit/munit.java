e689d9034850571d3a3975ce5241230d02c354f2
crazywolf132 <crazywolf132@gmail.com>
2019-05-11 13:24:22 +1000

    Removed old import

--- src/com/mani/lang/Modules/munit/munit.java
+++ src/com/mani/lang/Modules/munit/munit.java
@@ -10,8 +10,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class munit implements Module {
 
     @Override
