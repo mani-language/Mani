db525fda2bf6e4a3ba3e036fdc9be0ad74e426ed
crazywolf132 <crazywolf132@gmail.com>
2019-05-21 17:29:43 +1000

    Updated files_mkdir for new file structure

--- src/com/mani/lang/Modules/files/files_mkdir.java
+++ src/com/mani/lang/Modules/files/files_mkdir.java
@@ -1,7 +1,7 @@
 package com.mani.lang.Modules.files;
 
-import com.mani.lang.Interpreter;
-import com.mani.lang.ManiCallable;
+import com.mani.lang.core.Interpreter;
+import com.mani.lang.domain.ManiCallable;
 
 import java.io.IOException;
 import java.nio.file.Files;
