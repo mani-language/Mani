e620824144084f995d4a2f10beb750b02dc031bd
crazywolf132 <crazywolf132@gmail.com>
2019-05-21 17:32:43 +1000

    Updated maps_mapRemoveItem for new file structure

--- src/com/mani/lang/Modules/maps/maps_mapRemoveItem.java
+++ src/com/mani/lang/Modules/maps/maps_mapRemoveItem.java
@@ -1,7 +1,7 @@
 package com.mani.lang.Modules.maps;
 
-import com.mani.lang.Interpreter;
-import com.mani.lang.ManiCallable;
+import com.mani.lang.core.Interpreter;
+import com.mani.lang.domain.ManiCallable;
 
 import java.util.HashMap;
 import java.util.List;
