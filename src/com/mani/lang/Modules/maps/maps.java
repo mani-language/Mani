0995eccd4b47ac111d29401e0e2f901c6a9a4f84
crazywolf132 <crazywolf132@gmail.com>
2019-05-21 17:31:36 +1000

    Updated maps for new file structure

--- src/com/mani/lang/Modules/maps/maps.java
+++ src/com/mani/lang/Modules/maps/maps.java
@@ -1,8 +1,7 @@
 package com.mani.lang.Modules.maps;
 
-import com.mani.lang.Interpreter;
-import com.mani.lang.ManiCallable;
-import com.mani.lang.ManiCallableInternal;
+import com.mani.lang.core.Interpreter;
+import com.mani.lang.domain.ManiCallableInternal;
 import com.mani.lang.Modules.Module;
 
 import java.util.HashMap;
