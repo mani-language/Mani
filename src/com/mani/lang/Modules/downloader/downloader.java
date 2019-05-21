6ac83d241d3d60fffd8dab4929dddda7c45ece8e
crazywolf132 <crazywolf132@gmail.com>
2019-05-21 17:29:01 +1000

    Updated downloader for new file structure

--- src/com/mani/lang/Modules/downloader/downloader.java
+++ src/com/mani/lang/Modules/downloader/downloader.java
@@ -1,8 +1,8 @@
 package com.mani.lang.Modules.downloader;
 
-import com.mani.lang.Interpreter;
-import com.mani.lang.ManiCallable;
-import com.mani.lang.ManiFunction;
+import com.mani.lang.core.Interpreter;
+import com.mani.lang.domain.ManiCallable;
+import com.mani.lang.domain.ManiFunction;
 import com.mani.lang.Modules.Module;
 
 import javax.net.ssl.HttpsURLConnection;
