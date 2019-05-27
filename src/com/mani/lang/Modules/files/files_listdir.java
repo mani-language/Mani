f9c1b711652ca6c37b5675118d781049795b551e
crazywolf132 <crazywolf132@gmail.com>
2019-05-27 17:30:13 +1000

    Fixed listdir

--- src/com/mani/lang/Modules/files/files_listdir.java
+++ src/com/mani/lang/Modules/files/files_listdir.java
@@ -3,11 +3,14 @@ package com.mani.lang.Modules.files;
 import com.mani.lang.core.Interpreter;
 import com.mani.lang.domain.ManiCallable;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class files_listdir implements ManiCallable {
     @Override
@@ -17,12 +20,9 @@ public class files_listdir implements ManiCallable {
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
+        File f = new File((String) arguments.get(0));
         List<Object> db = new ArrayList<>();
-        try {
-            db = (List<Object>) Files.list(Paths.get((String) arguments.get(0)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        db = Arrays.asList(Objects.requireNonNull(f.listFiles()));
         return db;
     }
 }
