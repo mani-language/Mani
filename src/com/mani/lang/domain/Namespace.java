0289d881742604a78089c5802a075250e9570758
crazywolf132 <crazywolf132@gmail.com>
2019-06-20 14:18:46 +1000

    Added namespaces to project.

---
+++ src/com/mani/lang/domain/Namespace.java
@@ -0,0 +1,58 @@
+package com.mani.lang.domain;
+
+public class Namespace {
+
+    private String space;
+    private String name;
+
+    public Namespace(String space, String name) {
+        this.space = space;
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((space == null) ? 0 : space.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return  result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Namespace other = (Namespace) obj;
+        if (space == null) {
+            if (other.space != null) {
+                return false;
+            }
+        } else if (!space.equals(other.space)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Namespace {" +
+                "space='" + space + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
