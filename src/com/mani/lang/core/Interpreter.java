5277141a19400c8deae3737725cefd1e2581514e
jrickard <joerickard@hotmail.co.uk>
2019-07-18 12:59:07 +0100

    Added condition to use source branch stdlib if on CI #82

--- src/com/mani/lang/core/Interpreter.java
+++ src/com/mani/lang/core/Interpreter.java
@@ -391,7 +391,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
                 return "Already loaded!";
             } else {
                 try {
-                    URL url = new URL("https://raw.githubusercontent.com/crazywolf132/Mani/master/stdlib/" + (String) result + ".mni");
+                    String stdlibURL = "https://raw.githubusercontent.com/crazywolf132/Mani/master/stdlib/";
+                    if ("true".equals(System.getenv("CI"))){
+                        String sourceUser = System.getenv("CIRCLE_PROJECT_USERNAME");
+                        String sourceRepo = System.getenv("CIRCLE_PROJECT_REPONAME");
+                        String sourceBranch = System.getenv("CIRCLE_BRANCH");
+                        stdlibURL = "https://raw.githubusercontent.com/" + sourceUser + "/" + sourceRepo + "/" + sourceBranch + "/stdlib/";
+                    }
+                    URL url = new URL(stdlibURL + (String) result + ".mni");
                     Scanner s = new Scanner(url.openStream());
                     String final_file = "";
                     while(s.hasNextLine()){
