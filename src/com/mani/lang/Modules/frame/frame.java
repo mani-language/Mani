7bd06880a9108e0735a9da8d28349b1b58259a68
crazywolf132 <crazywolf132@gmail.com>
2019-07-31 15:30:55 +1000

    Added license to frame

--- src/com/mani/lang/Modules/frame/frame.java
+++ src/com/mani/lang/Modules/frame/frame.java
@@ -1,3 +1,13 @@
+/*
+ * This source file is part of the Máni open source project
+ *
+ * Copyright (c) 2018 - 2019.
+ *
+ * Licensed under Mozilla Public License 2.0
+ *
+ * See https://github.com/mani-language/Mani/blob/master/LICENSE.md for license information
+ */
+
 package com.mani.lang.Modules.frame;
 
 import com.mani.lang.core.Interpreter;
