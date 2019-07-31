bd03eea7c9e14c1ccf31a9161e30b62c36ade0bd
crazywolf132 <crazywolf132@gmail.com>
2019-07-31 15:16:28 +1000

    Added license to ManiClass

--- src/com/mani/lang/domain/ManiClass.java
+++ src/com/mani/lang/domain/ManiClass.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2019 This source file is part of the Máni open source project
+ *
+ * Copyright (c) 2018 - 2019.
+ *
+ * Licensed under Mozilla Public License 2.0
+ *
+ * See https://github.com/mani-language/Mani/blob/master/LICENSE.md for license information.
+ */
+
 package com.mani.lang.domain;
 
 import com.mani.lang.exceptions.RuntimeError;
