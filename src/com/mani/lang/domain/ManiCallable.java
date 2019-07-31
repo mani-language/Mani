b34347e326aa51aec0dac74214538480235b0d97
crazywolf132 <crazywolf132@gmail.com>
2019-07-31 15:16:03 +1000

    Added license to ManiCallable

--- src/com/mani/lang/domain/ManiCallable.java
+++ src/com/mani/lang/domain/ManiCallable.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2019 This source file is part of the Máni open source project
+ *
+ * Copyright (c) 2018 - 2019.
+ *
+ * Licensed under Mozilla Public License 2.0
+ *
+ * See https://github.com/mani-language/Mani/blob/master/LICENSE.md for license information.
+ */
 package com.mani.lang.domain;
 
 import com.mani.lang.core.Interpreter;
